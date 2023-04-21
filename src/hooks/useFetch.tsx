57938c8110786be99207c74e53c4845cdcfb2027
Dalex19 <dearalex875@gmail.com>
2023-04-21 12:21:40 -0600

    feat: Generate new requests when the page is reloaded.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -5,14 +5,19 @@ import { CharacterInfo } from "../service/getcharacter";
 function useFetch () {
     const [myCharacter, setMyCharacters] = useState<CharacterInfo[]>([]);
     
-    const getItemNow = async() => {
-        const promises = Array.from({ length: 5 }, (_, i) => getItems(i + 1));
-        const results = await Promise.all(promises);
-      
-        const filteredResults = results.filter((item): item is CharacterInfo => item !== null);
-      
-        setMyCharacters(filteredResults);
-    }
+    const getItemNow = async () => {
+      const randomNumbers = Array.from({ length: 5 }, () =>
+        Math.floor(Math.random() * 100 + 1)
+      );
+      const promises = randomNumbers.map((number) => getItems(number));
+      const results = await Promise.all(promises);
+
+      const filteredResults = results.filter(
+        (item): item is CharacterInfo => item !== null
+      );
+
+      setMyCharacters(filteredResults);
+    };
 
     useEffect(() => {
         getItemNow();
